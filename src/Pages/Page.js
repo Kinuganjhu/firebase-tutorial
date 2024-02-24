a64f8531d70dc3b2907f1b98d7a95acd535bdb99
Kinuganjhu <kinuganjhu02@gmail.com>
2024-02-24 11:52:06 +0530

    let start firebase db and firestre

--- src/Pages/Page.js
+++ src/Pages/Page.js
@@ -1,6 +1,4 @@
-import {
-  Link
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Profile from './Profile.svg';
 import Store from './Store.svg';
 
@@ -10,13 +8,14 @@ export default function Page() {
     <div className="page-container">
       <div className="link-container">
         <img src={Store} alt="Store" />
-      
+        <h1>Welcome to our Store</h1>
         <Link to='/Store'>Go To Store</Link>
-    </div>
+      </div>
       <div className="link-container">
         <img src={Profile} alt="Profile" />
+        <h1>Welcome to your Profile</h1>
         <Link to='/Profile'>Go To Profile</Link>
-  </div>
-</div>
-);
+      </div>
+    </div>
+  );
 }
