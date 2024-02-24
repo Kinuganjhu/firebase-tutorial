3834daa4722e9fad1c34d09198d6a13063bc1f39
Kinuganjhu <kinuganjhu02@gmail.com>
2024-02-24 09:52:13 +0530

    added image

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,8 @@ import { auth } from './Firebase';
 import { useState, useEffect } from 'react';
 import { GoogleAuthProvider, signInWithRedirect, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import log from './logo.svg';
+
 
 export default function Home() {
   const [error, setError] = useState(null);
@@ -17,13 +19,13 @@ export default function Home() {
     return () => unsubscribe();
   }, [navigate]);
 
-  const handleSign = () => {
+  const handleSign = (event) => {
+    event.preventDefault(); // Prevent the default behavior of the event
     const provider = new GoogleAuthProvider();
     signInWithRedirect(auth, provider)
       .then((result) => {
         alert('Sign up successful');
         navigate('/Page');
-        
       })
       .catch((error) => {
         setError(error.message);
@@ -33,8 +35,11 @@ export default function Home() {
   return (
     <div className='app'>
       <h1>Let's Get Started</h1>
-        {error && <p>{error}</p>}
-      <button onClick={handleSign}>Continue With Google</button>
+      <button className='sign-up-button' onClick={handleSign}>
+        <img src={log} alt='logo' height='24' width='24' />
+       &nbsp; Continue With Google
+      </button>
+      {error && <p>{error}</p>}
     </div>
   );
 }
