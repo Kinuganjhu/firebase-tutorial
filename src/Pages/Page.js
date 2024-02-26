58621bf3d8758b7c1b7f7860108ad1eff116c440
React Developer <136769553+Kinuganjhu@users.noreply.github.com>
2024-02-26 10:07:25 +0530

    Update Page.js

--- src/Pages/Page.js
+++ src/Pages/Page.js
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 import Profile from './Profile.svg';
 import Store from './Store.svg';
-
-
+import {Link} from 'react-router-dom'
 export default function Page() {
   return (
     <div className="page-container">
+    <Link to = '/'>Go Back </Link>
       <div className="link-container">
         <img src={Store} alt="Store" />
       
